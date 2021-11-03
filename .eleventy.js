af71b0f8d9f8c8557430daf23b2677fa26a05292
Steve Marks <stevenmarks75@gmail.com>
2021-11-03 22:31:14 +0000

    add shopify plugin

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,9 @@ const navigation = require("@11ty/eleventy-navigation")
 const seo = require("eleventy-plugin-seo")
 const unpkgInliner = require("eleventy-njk-unpkg-inliner")
 const site = require("./src/_data/site.js")
+const shopify = require("eleventy-plugin-shopify")
+require("dotenv").config()
+const { SHOPIFY_STORE_URL, SHOPIFY_ACCESS_TOKEN, SHOPIFY_API_VERSION } = process.env
 
 module.exports = function(eleventyConfig) {
 
@@ -10,6 +13,12 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addLayoutAlias('default', 'layouts/base.njk')
     
     // PLUGINS
+eleventyConfig.addPlugin(shopify, {
+    url: '',
+    key: '',
+    version: '',
+})
+
     eleventyConfig.addPlugin(pluginTailwindCSS, {
         src: "src/css/style.css"
     })
